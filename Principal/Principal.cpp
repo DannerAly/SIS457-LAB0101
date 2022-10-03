c5ad887f29658f4b888dbbb80bcffbd5fa48d2c1
Danner Abel Aly Delgado <dannerabelaly@gmail.com>
2022-10-02 20:32:36 -0400

    Agregar archivos de proyecto.

---
+++ Principal/Principal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "********  HOLA MUNDO  ********" << endl;
+	cout << "\nNombre y Apellido: Danner Abel Aly Delgado" << endl;
+	cout << "Telefono: 67914341" << endl;
+	cout << "Fecha de nacimiento: 25 de noviembre de 2002" << endl;
+	cout << "C.I: 12526225" << endl;
+	cout << "Solo un muchacho de 19 años amante a la producción audiovisual y la programación" << endl;
+	cout << "Desafio: En este semestre lograr cumplir con el desafío de la " << endl;
+	cout << "elaboración de un videojuego completo aunque me cueste y duela" << endl;
+	cout << "*********************************************************************" << endl;
+	return 0;
+}
